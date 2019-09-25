674daedb68bd99c25c9f22e193f73f1bf156d3ab
Yongkun Li <indusdesign@163.com>
2019-09-25 12:39:56 -0400

    convert BGsubtraction to class

---
+++ src/colorDetector.cpp
@@ -0,0 +1,8 @@
+//
+//  colorDetector.cpp
+//  SM01_YongkunLI
+//
+//  Created by Yongkun Li on 9/25/19.
+//
+
+#include "colorDetector.hpp"
---
+++ src/colorDetector.hpp
@@ -0,0 +1,22 @@
+//
+//  colorDetector.hpp
+//  SM01_YongkunLI
+//
+//  Created by Yongkun Li on 9/25/19.
+//
+
+#ifndef colorDetector_hpp
+#define colorDetector_hpp
+
+#include "ofMain.h"
+#include "ofxCv.h"
+#include "ofxGui.h"
+
+#include <stdio.h>
+
+#endif /* colorDetector_hpp */
+
+class colorDetector{
+public:
+    
+};
